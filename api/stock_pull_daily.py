b6e10aed5e2b8d951262c10663b24889d62404c5
naiive <naiivebeluga@gmail.com>
2025-12-18 16:44:00 +0800

    修正

--- api/stock_pull_daily.py
+++ api/stock_pull_daily.py
@@ -452,15 +452,15 @@ def main():
 
 if __name__ == "__main__":
 
-    # 有数据就执行 main，没有数就等有数据后在执行
+    # 今天有数据就执行 main，么有数就等有数据后在执行
     # 一般16:00开始有数据
+    today = datetime.now().strftime("%Y%m%d")
     if ak.stock_zh_a_daily(
         symbol="sh600519",
-        start_date= CONFIG["TARGET_START_DATE"],
-        end_date= CONFIG["TARGET_END_DATE"],
+        start_date=today,
+        end_date=today,
         adjust="qfq"
     ).empty:
-        date = CONFIG["TARGET_START_DATE"]
-        logger.warning(f"✅今天 {date}还没数据，等【ak.stock_zh_a_daily】接口有数据在执行！")
+        logger.warning(f"✅今天 {today}还没数据，等【ak.stock_zh_a_daily】接口有数据在执行！")
     else:
         main()
