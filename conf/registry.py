77cbdccbe66a7627955c902913c1e95a35ac16be
naiive <naiivebeluga@gmail.com>
2025-12-25 17:36:03 +0800

    修改策略名称

--- conf/registry.py
+++ conf/registry.py
@@ -2,17 +2,17 @@
 
 # 策略注册表
 from strategies import (
+    cross_strategy,
     squeeze_resistance_strategy,
     squeeze_adx_strategy,
-    cross_strategy,
-    maroon_squeeze_strategy,
+    squeeze_maroon_strategy,
     fair_value_gap_strategy
 )
 
 STRATEGY_REGISTRY = {
+    "cross": cross_strategy.run_strategy,
     "squeeze_resistance": squeeze_resistance_strategy.run_strategy,
     "squeeze_adx": squeeze_adx_strategy.run_strategy,
-    "cross": cross_strategy.run_strategy,
-    "maroon_squeeze": maroon_squeeze_strategy.run_strategy,
+    "squeeze_maroon": squeeze_maroon_strategy.run_strategy,
     "fvg": fair_value_gap_strategy.run_strategy,
 }
