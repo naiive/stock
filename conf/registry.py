65f80b66219bc81274939e0feab20d63dd4a059a
naiive <naiivebeluga@gmail.com>
2025-12-28 12:06:28 +0800

    策略自定义打分⭐⭐⭐⭐⭐

--- conf/registry.py
+++ conf/registry.py
@@ -14,22 +14,22 @@ from strategies import (
 )
 
 STRATEGY_REGISTRY = {
-    # 原生squeeze 策略
+    # 原生squeeze 策略【⭐⭐⭐⭐】
     "squeeze": squeeze_strategy.run_strategy,
-    # squeeze + adx > 25 策略
+    # squeeze + adx > 25 策略【⭐⭐⭐⭐⭐】
     "squeeze_adx": squeeze_adx_strategy.run_strategy,
-    # squeeze + 突破前高 策略
+    # squeeze + 突破前高 策略【⭐⭐⭐⭐⭐】
     "squeeze_resistance": squeeze_resistance_strategy.run_strategy,
-    # squeeze 红色覆盖挤压释放 策略
+    # squeeze 红色覆盖挤压释放 策略【⭐⭐⭐】
     "squeeze_red_cover": squeeze_red_cover_strategy.run_strategy,
-    # squeeze 底背离 策略
+    # squeeze 底背离 策略【⭐⭐】
     "squeeze_divergence": squeeze_divergence_strategy.run_strategy,
-    # macd 双峰底背离 策略
+    # macd 双峰底背离 策略【⭐⭐⭐⭐⭐】
     "macd_histogram_double_divergence": macd_histogram_double_divergence_strategy.run_strategy,
-    # 交叉买入型号 策略
+    # 交叉买入型号 策略【⭐⭐⭐⭐】
     "cross": cross_strategy.run_strategy,
-    # 底部-100是否新低 策略
+    # 底部-100是否新低 策略【⭐⭐⭐】
     "aroon_oscillator": aroon_oscillator_strategy.run_strategy,
-    # 是否有fvg
+    # 是否有fvg 策略【⭐⭐】
     "fvg": fair_value_gap_strategy.run_strategy
 }
