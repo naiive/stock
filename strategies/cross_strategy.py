25d88cd49b9093e6a9f3e50c3cef130bcfeac906
naiive <naiivebeluga@gmail.com>
2026-01-01 16:44:48 +0800

    策略cross_stragtegy修改

--- strategies/cross_strategy.py
+++ strategies/cross_strategy.py
@@ -9,8 +9,7 @@ def run_strategy(df, symbol):
     """
     A股全市场扫描策略
     策略：
-         cross 为 green 且在 超卖区 wtc_red < -53 and wtc_green < -53
-
+         cross 为 green 且在 超卖区 wtc_red < -45
     :param df: DataFrame
     :param symbol: 股票代码
     :return: 命中则返回字典，未命中返回 None
@@ -25,8 +24,6 @@ def run_strategy(df, symbol):
         prev_close = float(df['close'].iloc[-2])
         pct_chg = (current_close - prev_close) / prev_close * 100
 
-        # 3. 策略计算，一定要断点，就是先把简单的指标计算，不满足就跳过后面复杂的计算
-
         # 3.1 涨幅大于等于0
         if pct_chg <= 0:
             return None
@@ -39,37 +36,24 @@ def run_strategy(df, symbol):
 
         # 3.4 crosses
         df = wavetrend_with_crosses_indicator(df)
-        last = df.iloc[-1]
-
-        wtc_green = pd.to_numeric(last.get('wtc_green'), errors='coerce')
-        wtc_red = pd.to_numeric(last.get('wtc_red'), errors='coerce')
-
-        wtc_signal = last.get('wtc_signal')
+        current = df.iloc[-1]
+        wtc_green = pd.to_numeric(current.get('wtc_green'), errors='coerce')
+        wtc_red = pd.to_numeric(current.get('wtc_red'), errors='coerce')
+        wtc_signal = current.get('wtc_signal')
 
         # 金叉
-        signal = None
-        if wtc_signal == 'green' and wtc_red < -53 and wtc_green < -53 :
-            signal = "买入"
-
-        # 返回结果
-        if signal == "买入":
-            # 4. 只有信号触发，才计算 ATR 止损
-            df = atr_indicator(df)
-            last_atr = df.iloc[-1]
-            trade_date = str(last.get('date'))
+        if wtc_signal == 'green' and wtc_red < -45:
             return {
-                "日期": trade_date,
+                "日期": current.name.strftime('%Y-%m-%d'),
                 "代码": symbol,
-                "当前价": round(current_close, 2),
+                "现价": round(current_close, 2),
                 "涨幅(%)": round(pct_chg, 2),
                 "EMA200": round(ema200, 2),
-                "建议止损价": round(last_atr.get('atr_long_stop'), 2),
                 "绿线": round(wtc_green, 2),
-                "红线": round(wtc_red, 2),
+                "红线": round(wtc_red, 2)
             }
 
-    except Exception as e:
-        # 这里不需要打印，错误会抛给 _worker 的 try...except
+    except Exception:
         return None
 
     return None
