70ed61eb2326f61b8eb53362e6cda6c30a668287
naiive <naiivebeluga@gmail.com>
2026-01-01 20:36:14 +0800

    降级

--- conf/registry.py
+++ conf/registry.py
@@ -12,11 +12,11 @@ from strategies import (
 STRATEGY_REGISTRY = {
     # squeeze + 突破前高 策略【⭐⭐⭐⭐⭐】
     "squeeze_resistance": squeeze_resistance_strategy.run_strategy,
-    # squeeze + adx > 25 策略【⭐⭐⭐⭐⭐】
+    # squeeze + adx > 25 策略【⭐⭐⭐⭐】
     "squeeze_adx": squeeze_adx_strategy.run_strategy,
-    # macd 双峰底背离 策略【⭐⭐⭐⭐】
+    # macd 双峰底背离 策略【⭐⭐⭐】
     "macd_histogram_double_divergence": macd_histogram_double_divergence_strategy.run_strategy,
-    # 交叉买入型号 策略【⭐⭐⭐⭐】
+    # 交叉买入型号 策略【⭐⭐⭐】
     "cross": cross_strategy.run_strategy,
     # 底部是否新低 策略【⭐⭐⭐】
     "aroon_oscillator": aroon_oscillator_strategy.run_strategy
