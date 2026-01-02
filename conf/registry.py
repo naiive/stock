765ec2f311183b7a42be04c9a07b9be2987c32b0
naiive <naiivebeluga@gmail.com>
2026-01-02 14:30:59 +0800

    删除squeeze_adx策略

--- conf/registry.py
+++ conf/registry.py
@@ -3,7 +3,6 @@
 # 策略注册表
 from strategies import (
     squeeze_resistance_strategy,
-    squeeze_adx_strategy,
     macd_histogram_double_divergence_strategy,
     cross_strategy,
     aroon_oscillator_strategy
@@ -12,8 +11,6 @@ from strategies import (
 STRATEGY_REGISTRY = {
     # squeeze + 突破前高 策略【⭐⭐⭐⭐⭐】
     "squeeze_resistance": squeeze_resistance_strategy.run_strategy,
-    # squeeze + adx > 25 策略【⭐⭐⭐⭐】
-    "squeeze_adx": squeeze_adx_strategy.run_strategy,
     # macd 双峰底背离 策略【⭐⭐⭐】
     "macd_histogram_double_divergence": macd_histogram_double_divergence_strategy.run_strategy,
     # 交叉买入型号 策略【⭐⭐⭐】
