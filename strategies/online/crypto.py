a98631bb1f508d6be07df7a042fa1d6742fa6744
naiive <naiivebeluga@gmail.com>
2026-01-05 21:18:16 +0800

    crypto调整触发条件

--- strategies/online/crypto.py
+++ strategies/online/crypto.py
@@ -396,9 +396,16 @@ class StrategyEngine:
 
         signal = "No"
         if cur['sqz_status'] == "OFF" and prev['sqz_status'] == "ON" and prev['sqz_id'] >= self.cfg['min_sqz_bars']:
-            if cur['close'] > cur['ema200'] and change > 0 and cur['sqz_hcolor'] == "亮绿":
+            if (cur['close'] > cur['ema200']
+                    and change > 0
+                    and cur['close'] > cur['srb_res']
+                    and cur['sqz_hcolor'] == "亮绿"):
                 signal = "Long"
-            elif cur['close'] < cur['ema200'] and change < 0 and cur['sqz_hcolor'] == "亮红":
+
+            elif (cur['close'] < cur['ema200']
+                  and change < 0
+                  and cur['close'] < cur['srb_sup']
+                  and cur['sqz_hcolor'] == "亮红"):
                 signal = "Short"
 
         energy, tr, ts = [], [], []
